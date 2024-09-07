51ab81a444c1744cadf6c70d8c1dd62e4c436fe7
xznayarax_0 <de.lima.nayara@escola.pr.gov.br>
2024-09-06 22:37:09 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+     background("white");
+  }
+  
+  
+  function draw() {
+    
+    stroke ("blue");
+    fill ("red");
+   
+    
+    // console.log(mouseIsPressed);
+    
